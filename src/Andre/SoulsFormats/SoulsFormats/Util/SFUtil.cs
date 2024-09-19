ffc8719c135ab3448caba8bf819f9ddd7271356c
Philiquaz <Philiquaz@gmail.com>
2024-09-19 04:31:21 +0100

    Span

--- src/Andre/SoulsFormats/SoulsFormats/Util/SFUtil.cs
+++ src/Andre/SoulsFormats/SoulsFormats/Util/SFUtil.cs
@@ -327,12 +327,12 @@ namespace SoulsFormats
                 return decompressedStream.ToArray();
             }
         }
-        public static byte[] WriteZstd(byte[] data, int compressionLevel)
+        public static byte[] WriteZstd(Span<byte> data, int compressionLevel)
         {
             var options = new CompressionOptions(null, new Dictionary<ZSTD_cParameter, int> { { ZSTD_cParameter.ZSTD_c_contentSizeFlag, 0 }, { ZSTD_cParameter.ZSTD_c_windowLog, 16 } }, compressionLevel);
             using (var compressor = new Compressor(options))
             {
-                return compressor.Wrap(data).ToArray();
+                return compressor.Wrap(data);
             }
         }
         public static byte[] ReadZstd(BinaryReaderEx br, int compressedSize)
